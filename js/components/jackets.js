f7eb1d4c7c510183c3c641b89db1d9d2d293e07d
MariusChristensen <91680871+MariusChristensen@users.noreply.github.com>
2022-03-25 21:50:37 +0100

    don't even know what i'm doing anymore
    the fuck
    
    - Think my modal is working fine, might tweak some background styling to make it look better. having my entire form in a innerhtml looks horrible, not sure what to do.
    - Fixed multiple links all over
    - Added icons to hamburger menu on all pages
    - Added an export js file to use for jacket objects, maybe API stuff later on.
    - Fixed some typos
    - Added font to a button that was suddenly missing it? I don't even know at this point.
    
    - I feel dumb
    - Thanks

---
+++ js/components/jackets.js
@@ -0,0 +1,11 @@
+export { jacket };
+
+const jacket = {
+  name: "RainyDays All-Star Jacket",
+  gender: "Female",
+  price: "79.99$",
+  image: "/images/shop-female2.jpg",
+  description:
+    "The RainyDays All-Star Jacket for women is specifically developed for anyone interested in being outdoors 24/7. Wether it be hiking, exploring, skiing, camping or canoeing, we got you covered!",
+  tagline: "Durable and soft on the wallet, what more could you ever want? Get yours now!",
+};
